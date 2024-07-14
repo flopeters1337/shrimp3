b4509d1a18879ae8bcceabc6ccbc88d888709d7a
Florian Peters <fl0wryan@hotmail.com>
2024-07-14 17:38:19 +0100

    Refactor tab selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
+import {ReactNode, useState} from "react";
 
 /** Mantine specific imports and constants */
 import '@mantine/core/styles.css';
-import {AppShell, Burger, Button, Group, NavLink, Stack, MantineProvider} from '@mantine/core';
+import {AppShell, Burger, Group, MantineProvider, NavLink, Stack} from '@mantine/core';
 import {useDisclosure} from "@mantine/hooks";
 import {IconAdjustments, IconPlaylist, IconSpeakerphone} from "@tabler/icons-react";
 
@@ -10,6 +11,50 @@ import {IconAdjustments, IconPlaylist, IconSpeakerphone} from "@tabler/icons-rea
 function App() {
     /** UI features */
     const [burgerOpened, {toggle: toggleBurger}] = useDisclosure();
+    const [currentTab, setCurrentTab] = useState('playback');
+    const appTabs = [
+        {
+            tabName: "playback",
+            label: "Audio Playback",
+            icon: (<IconPlaylist size="1rem"/>)
+        },
+        {
+            tabName: "soundboard",
+            label: "Soundboard",
+            icon: (<IconSpeakerphone size="1rem"/>)
+        },
+        {
+            tabName: "settings",
+            label: "Settings",
+            icon: (<IconAdjustments size="1rem"/>)
+        },
+    ];
+
+    function createNavLink(tabName: string, label: string, icon: ReactNode)
+    {
+        return (
+            <NavLink
+                href="#required-for-focus"
+                label={label}
+                leftSection={icon}
+                onClick={() => setCurrentTab(tabName)}
+                active={currentTab === tabName}
+            ></NavLink>
+        );
+    }
+
+    function renderCurrentTab(currentTab: string) {
+        switch (currentTab) {
+            case "playback":
+                return (<p>Select current playlist here.</p>);
+            case "soundboard":
+                return (<p>Play soundboard cues here.</p>);
+            case "settings":
+                return (<p>Change settings here.</p>);
+            default:
+                return (<p>Unknown app tab.</p>);
+        }
+    }
 
     return (
         <MantineProvider defaultColorScheme={"dark"}>
@@ -29,31 +74,13 @@ function App() {
                 </AppShell.Header>
 
                 <AppShell.Navbar>
-                    <Stack
-                        gap="0"
-                    >
-                        <NavLink
-                            href="#required-for-focus"
-                            label="Audio Playback"
-                            leftSection={<IconPlaylist size="1rem"/>}
-                        ></NavLink>
-                        <NavLink
-                            href="#required-for-focus"
-                            label="Soundboard"
-                            leftSection={<IconSpeakerphone size="1rem"/>}
-                        ></NavLink>
-                        <NavLink
-                            href="#required-for-focus"
-                            label="Settings"
-                            leftSection={<IconAdjustments size="1rem"/>}
-                        ></NavLink>
+                    <Stack gap="0">
+                        {appTabs.map((tab) => createNavLink(tab.tabName, tab.label, tab.icon))}
                     </Stack>
                 </AppShell.Navbar>
 
                 <AppShell.Main>
-                    <Button>
-                        Toggle Navbar
-                    </Button>
+                    { renderCurrentTab(currentTab) }
                 </AppShell.Main>
             </AppShell>
         </MantineProvider>
