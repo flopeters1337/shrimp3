c8cbad1a8f17a88f85b626dfa96320254e40b4f2
Florian Peters <fl0wryan@hotmail.com>
2024-07-14 18:04:58 +0100

    Add 'Ambient Sounds' tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,54 @@ import {ReactNode, useState} from "react";
 import '@mantine/core/styles.css';
 import {AppShell, Burger, Group, MantineProvider, NavLink, Stack} from '@mantine/core';
 import {useDisclosure} from "@mantine/hooks";
-import {IconAdjustments, IconPlaylist, IconSpeakerphone} from "@tabler/icons-react";
+import {IconAdjustments, IconPlaylist, IconSpeakerphone, IconTrees} from "@tabler/icons-react";
+
+enum Tab {
+    Playback,
+    Ambient,
+    Soundboard,
+    Settings,
+}
+
+interface TabMetadata {
+    tab: Tab;
+    label: string;
+    icon: ReactNode;
+}
 
 /** Main app renderer */
 function App() {
     /** UI features */
     const [burgerOpened, {toggle: toggleBurger}] = useDisclosure();
-    const [currentTab, setCurrentTab] = useState('playback');
-    const appTabs = [
-        {
-            tabName: "playback",
-            label: "Audio Playback",
-            icon: (<IconPlaylist size="1rem"/>)
-        },
-        {
-            tabName: "soundboard",
-            label: "Soundboard",
-            icon: (<IconSpeakerphone size="1rem"/>)
-        },
-        {
-            tabName: "settings",
-            label: "Settings",
-            icon: (<IconAdjustments size="1rem"/>)
-        },
+    const [currentTab, setCurrentTab] = useState<Tab>(Tab.Playback);
+    const appTabs: TabMetadata[] = [
+        {tab: Tab.Playback, label: "Audio Playback", icon: (<IconPlaylist size="1rem"/>)},
+        {tab: Tab.Ambient, label: "Ambient Sounds", icon: (<IconTrees size="1rem"/>)},
+        {tab: Tab.Soundboard, label: "Soundboard", icon: (<IconSpeakerphone size="1rem"/>)},
+        {tab: Tab.Settings, label: "Settings", icon: (<IconAdjustments size="1rem"/>)},
     ];
 
-    function createNavLink(tabName: string, label: string, icon: ReactNode)
-    {
+    function createNavLink(tabMetadata: TabMetadata) {
         return (
             <NavLink
                 href="#required-for-focus"
-                label={label}
-                leftSection={icon}
-                onClick={() => setCurrentTab(tabName)}
-                active={currentTab === tabName}
+                label={tabMetadata.label}
+                leftSection={tabMetadata.icon}
+                onClick={() => setCurrentTab(tabMetadata.tab)}
+                active={currentTab === tabMetadata.tab}
             ></NavLink>
         );
     }
 
-    function renderCurrentTab(currentTab: string) {
+    function renderCurrentTab(currentTab: Tab) {
         switch (currentTab) {
-            case "playback":
+            case Tab.Playback:
                 return (<p>Select current playlist here.</p>);
-            case "soundboard":
+            case Tab.Ambient:
+                return (<p>Select ambient sounds here.</p>)
+            case Tab.Soundboard:
                 return (<p>Play soundboard cues here.</p>);
-            case "settings":
+            case Tab.Settings:
                 return (<p>Change settings here.</p>);
             default:
                 return (<p>Unknown app tab.</p>);
@@ -75,12 +78,12 @@ function App() {
 
                 <AppShell.Navbar>
                     <Stack gap="0">
-                        {appTabs.map((tab) => createNavLink(tab.tabName, tab.label, tab.icon))}
+                        {appTabs.map((tab) => createNavLink(tab))}
                     </Stack>
                 </AppShell.Navbar>
 
                 <AppShell.Main>
-                    { renderCurrentTab(currentTab) }
+                    {renderCurrentTab(currentTab)}
                 </AppShell.Main>
             </AppShell>
         </MantineProvider>
