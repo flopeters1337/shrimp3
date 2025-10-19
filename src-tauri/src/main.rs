6af84bc6d87cf962475c196d7c02c61d6abb1bcf
Florian Peters <fl0wryan@hotmail.com>
2025-10-19 18:45:38 +0100

    Update Cargo file + Cleanup `main.rs`

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
+use tauri::{Builder, command, generate_handler, generate_context};
+
+#[command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+/* Main function */
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+    Builder::default()
+        .invoke_handler(generate_handler![greet])
+        .run(generate_context!())
         .expect("error while running tauri application");
 }
